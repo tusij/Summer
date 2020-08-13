fd12834f0d03c429bee65f572963a068a15275e2
赖志宇 <scauyu2017@163.com>
2020-08-13 15:27:22 +0800

    初始

---
+++ src/main/java/com/jiwei/system/pojo/Task.java
@@ -0,0 +1,117 @@
+package com.jiwei.system.pojo;
+
+import java.io.Serializable;
+
+public class Task implements Serializable {
+    private String id;
+    private String name;
+    private String sex;
+    private String telephone;
+    private String address;
+    private String description;
+    private String computerType;
+    private String status;
+    private String repairMethod;
+    private String startDate;
+    private String endDate;
+
+    public Task() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getComputerType() {
+        return computerType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getRepairMethod() {
+        return repairMethod;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setComputerType(String computerType) {
+        this.computerType = computerType;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setRepairMethod(String repairMethod) {
+        this.repairMethod = repairMethod;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", address='" + address + '\'' +
+                ", description='" + description + '\'' +
+                ", computerType='" + computerType + '\'' +
+                ", status='" + status + '\'' +
+                ", repairMethod='" + repairMethod + '\'' +
+                ", startDate='" + startDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                '}';
+    }
+}
